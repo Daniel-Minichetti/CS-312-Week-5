d795bed6e1210ce3e56e9a1856757ec2faad21f5
Daniel Minichetti <daniel.minichetti@gmail.com>
2024-07-10 20:54:04 -0700

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <footer>
+            <p>Copyright © Daniel Minichetti {currentYear}</p>
+        </footer>
+    );
+}
+
+export default Footer;
